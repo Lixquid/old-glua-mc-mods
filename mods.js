2904417f9466d26c7e43ad1effeb6f670dc207e5
Jon Howell <Lixquid@users.noreply.github.com>
2016-12-10 13:27:35 +0000

    Update mods.js

--- mods.js
+++ mods.js
@@ -15,6 +15,9 @@ glua.team.addMod("Draconic Evolution","https://minecraft.curseforge.com/projects
 glua.team.addMod("Iron Chests","https://minecraft.curseforge.com/projects/iron-chests");
 glua.team.addMod("Iron Backpacks","https://minecraft.curseforge.com/projects/iron-backpacks");
 glua.team.addMod("Ranged Pumps","https://minecraft.curseforge.com/projects/ranged-pumps");
+glua.team.addMod("Super Crafting Frame","https://mods.curse.com/mc-mods/minecraft/super-crafting-frame");
+glua.team.addMod("Tinker's Construct","https://mods.curse.com/mc-mods/minecraft/tinkers-construct");
+
 
 glua.team.addClientMod("Inventory Sorter","https://minecraft.curseforge.com/projects/inventory-sorter");
 glua.team.addClientMod("JEI","https://minecraft.curseforge.com/projects/just-enough-items-jei");
